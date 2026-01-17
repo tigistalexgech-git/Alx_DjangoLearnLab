17dab5cb46bc7454e463735b9df1f9de9b688d6e
tigistalexgech-git <tigistalexgech@gmail.com>
2026-01-17 22:06:45 +0300

    Fix query_samples to match ALX expected ORM queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,31 +1,12 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query 1: All books by a specific author
-def get_books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        return author.books.all()
-    except Author.DoesNotExist:
-        return "Author not found"
+# Query all books by a specific author
+author = Author.objects.get(name="Chinua Achebe")
+books_by_author = Book.objects.filter(author=author)
 
+# List all books in a library
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()
 
-# Query 2: List all books in a library
-def get_books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return "Library not found"
-
-
-# Query 3: Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.librarian
-    except Library.DoesNotExist:
-        return "Library not found"
-    except Librarian.DoesNotExist:
-        return "No librarian assigned"
-
-
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
