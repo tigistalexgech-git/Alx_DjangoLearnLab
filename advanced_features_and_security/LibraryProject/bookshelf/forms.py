917854736c894b6e9cc6478e80d9c2cf8ef80c26
tigistalexgech-git <tigistalexgech@gmail.com>
2026-01-25 21:29:28 +0300

    Update form.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,19 @@
 
 from django import forms
 from .models import Article
+from .models import Book
 
-class ArticleForm(forms.ModelForm):
+
+class ExampleForm(forms.ModelForm):
+    """
+    ExampleForm is used to demonstrate secure form handling,
+    CSRF protection, and input validation.
+    """
     class Meta:
-        model = Article
-        fields = ['title', 'content']
+        model = Book
+        fields = ['title', 'author']
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
