7344bc5018ebe299c14b72650b39c9dca4f9219c
tigistalexgech-git <tigistalexgech@gmail.com>
2026-02-23 03:31:48 +0300

    Blog Post Management Featuress

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,12 +3,12 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 uurlpatterns = [
-    path('posts/', views.PostListView.as_view(), name='post-list'),
+   path('posts/', views.PostListView.as_view(), name='post-list'),
     path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-
+    
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
 
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login
 from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from django.db.models import Q
@@ -45,29 +46,34 @@ def search(request):
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
     model = Post
+    template_name = 'blog/post_detail.html'
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
 
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
 
-
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
+    template_name = 'blog/post_confirm_delete.html'
     success_url = '/posts/'
 
     def test_func(self):
