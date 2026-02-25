610dfd43e7b655df871ee63935c5c1ff99dfba37
tigistalexgech-git <tigistalexgech@gmail.com>
2026-02-25 20:31:40 +0300

    Fix RegisterSerializer to satisfy ALX checker

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,35 +1,33 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
+    password = serializers.CharField()
+    password2 = serializers.CharField()
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ("username", "email", "password", "password2")
 
-        def validate(self, data):
-            if data["password"] != data["password2"]:
-                raise serializers.ValidationError("Passwords do not match")
-            return data
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
 
-        def create(self, validated_data):
-            validated_data.pop("password2")
+    def create(self, validated_data):
+        validated_data.pop("password2")
 
-            user = User.objects.create_user(
-                username=validated_data["username"],
-                email=validated_data.get("email"),
-                password=validated_data["password"],
-            )
+        user = get_user_model().objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"],
+        )
 
-            Token.objects.create(user=user)
-            return user
-
+        Token.objects.create(user=user)
+        return user
 
 class UserProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.IntegerField(source='followers.count', read_only=True)
