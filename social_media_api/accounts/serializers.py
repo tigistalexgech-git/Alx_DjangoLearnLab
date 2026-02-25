43baae47ac64c85f41a9ec869ed656fefa81f81f
tigistalexgech-git <tigistalexgech@gmail.com>
2026-02-25 22:44:23 +0300

    Implement posts and comments CRUD with permissions and filtering

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,32 +2,34 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
+
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField()
-    password2 = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ("username", "email", "password", "password2")
 
-    def validate(self, data):
-        if data["password"] != data["password2"]:
-            raise serializers.ValidationError("Passwords do not match")
-        return data
+        def validate(self, data):
+            if data["password"] != data["password2"]:
+                raise serializers.ValidationError("Passwords do not match")
+            return data
 
-    def create(self, validated_data):
-        validated_data.pop("password2")
+        def create(self, validated_data):
+            validated_data.pop("password2")
 
-        user = get_user_model().objects.create_user(
-            username=validated_data["username"],
-            email=validated_data.get("email"),
-            password=validated_data["password"],
-        )
+            user = User.objects.create_user(
+                username=validated_data["username"],
+                email=validated_data.get("email"),
+                password=validated_data["password"],
+            )
 
-        Token.objects.create(user=user)
-        return user
+            Token.objects.create(user=user)
+            return user
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.IntegerField(source='followers.count', read_only=True)
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -9,8 +9,7 @@ from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.views import APIView
-from django.contrib.auth import authenticate
-from rest_framework.permissions import AllowAny
+
 from rest_framework.permissions import IsAuthenticated
 
 
