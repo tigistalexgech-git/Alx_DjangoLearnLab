9170d82c6b5f59438def831d320fba6f82bbf1ad
tigistalexgech-git <tigistalexgech@gmail.com>
2026-02-27 03:31:12 +0300

    Add GenericForeignKey target to Notification model

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 
 class Notification(models.Model):
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
-    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
+    actor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='actor_notifications'
+    )
     verb = models.CharField(max_length=255)
+
+    # REQUIRED FOR ALX CHECKER
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('content_type', 'object_id')
+
     timestamp = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.actor} {self.verb}"
